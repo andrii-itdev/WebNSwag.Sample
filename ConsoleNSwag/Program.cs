e3e9b5bdd7a74accf4281ce4b32a8623d83e229d
andrii-itdev <andrii.itdev@gmail.com>
2022-12-21 23:40:34 +0200

    Recreated project; Generated Client

---
+++ ConsoleNSwag/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using MyNamespace;
+
+Console.WriteLine("Welcome to Nswag Generated Client");
+
+
+var testClient = new Client("https://localhost:7181");
+
+var results = testClient.GetWeatherForecastAsync().GetAwaiter().GetResult();
+
+//var getAllresult = testClient.GetAllAsync().GetAwaiter().GetResult();
+
+results.ToList().ForEach(
+    x => Console.WriteLine(@$"Date: {x.Date}\tTemp: {x.TemperatureC}\tSummary: {x.Summary}")
+);
